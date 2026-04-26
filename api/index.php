ae9238597525ffdedc7462820827dd556efd960e
Suprapto <suprapto.kulo@gmail.com>
2026-04-26 12:34:06 +0700

    Fix tmp directory for Vercel compiled views

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
+// Create tmp directory for compiled views
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+if (!is_dir('/tmp/cache')) {
+    mkdir('/tmp/cache', 0755, true);
+}
+
 chdir(dirname(__DIR__));
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
